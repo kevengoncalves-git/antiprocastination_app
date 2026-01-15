0c6727434a9705b29bdb4d9d7b26dcaf6b845846
Keven <kevenkaue25@gmail.com>
2026-01-14 21:53:23 -0300

    Adição do fundo inicial

--- main_app.py
+++ main_app.py
@@ -1,1 +1,12 @@
 print("Bem vindo ao app antiprocastinação!")
+
+from tkinter import *
+
+janela_principal = Tk()
+janela_principal.title("App antiprocastinação")
+janela_principal.geometry('400x320')
+janela_principal.config(bg='#0f132e')
+janela_principal.resizable(True, True)
+
+
+janela_principal.mainloop()
