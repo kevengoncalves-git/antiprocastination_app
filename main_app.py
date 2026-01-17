2d2bacc21f151c66a6ffd3039276fd4ecfaa368d
Keven <kevenkaue25@gmail.com>
2026-01-17 09:57:49 -0300

    Adição dos frames de conteúdos principais e de controle temporal

--- main_app.py
+++ main_app.py
@@ -1,15 +1,17 @@
-print("Bem vindo ao app antiprocastinação!")
+print("Bem vindo ao app Anti-Procastinação!")
 
 from tkinter import *
 
 janela_principal = Tk()
-janela_principal.title("App antiprocastinação")
-janela_principal.geometry('500x370')
+janela_principal.title("App Anti-Procastinação")
+janela_principal.geometry('600x450')
 janela_principal.config(bg='#0f132e')
 janela_principal.resizable(True, True)
 
-frame_principal = Frame(janela_principal,bg='#536D88', height=300, width=300)
+#Frame de origem
+frame_principal = Frame(janela_principal,bg='#536D88', height=500, width=400)
 frame_principal.pack(pady=10, padx=10, fill='both', expand=True)
+frame_principal.pack_propagate(False)
 
 # 1. Widget de Texto
 label_titulo = Label(
@@ -20,6 +22,15 @@ label_titulo = Label(
     font=("Verdana", 14, "bold"), 
     relief='sunken'
 )
-label_titulo.pack(pady=20, padx=10)
+label_titulo.pack(pady=10)
+
+# 2. Widget de conteúdos principal
+frame_conteudo_principal = Frame(frame_principal, bg='#D9D9D9', height=310, width=380)
+frame_conteudo_principal.pack(pady=1)
+frame_conteudo_principal.pack_propagate(False) # Impede o frame de encolher
+
+# 2.1 Menu de controle de tempo
+frame_contole_tempo = Frame(frame_conteudo_principal, bg='#9B9B9B', height=160, width=250)
+frame_contole_tempo.pack(pady=10)
 
 janela_principal.mainloop()
